b7b029fdb1a07d97ba3a2e8be7913555ced421a2
Matthias Kloß <m.kloss@schule-mv.de>
2026-06-07 20:31:43 +0200

    v0.97

--- src/App.jsx
+++ src/App.jsx
@@ -183,7 +183,7 @@ if (showModal) {
       
       <div>
         <h1 className="text-2xl font-bold">Beobachtungsprotokoll</h1>
-        <p className="text-xs text-blue-200 -mt-1">v0.96b</p>
+        <p className="text-xs text-blue-200 -mt-1">v0.97</p>
       </div>
     </div>
 
